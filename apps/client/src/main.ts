e3a65b1e5bb6efb5f2f0aaeca5c5c805514090e8
Yuta Ohira <50891407+Alesion30@users.noreply.github.com>
2024-12-22 17:15:09 +0900

    Update client package configuration and implement API client for weather data

--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -1,4 +1,57 @@
-const main = () => {
-  console.log("Hello MCP Client");
+import { Client } from "@modelcontextprotocol/sdk/client/index.js";
+import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
+import {
+  CallToolResultSchema,
+  ListToolsResultSchema,
+} from "@modelcontextprotocol/sdk/types.js";
+
+const main = async () => {
+  const transport = new StdioClientTransport({
+    command: "node",
+    args: [
+      "/Users/s20814/work/sandbox/mcp-example/apps/weather-server/dist/main.cjs",
+    ],
+  });
+
+  const client = new Client(
+    {
+      name: "client",
+      version: "1.0.0",
+    },
+    {
+      capabilities: {},
+    }
+  );
+
+  console.log("Connecting to server...");
+  await client.connect(transport);
+  console.log("Connected to server!");
+
+  // 利用可能なtool一覧を取得する
+  const tools = await client.request(
+    { method: "tools/list" },
+    ListToolsResultSchema
+  );
+  console.log("Tools:", JSON.stringify(tools, null, 2));
+
+  // 天気予報のtoolを呼び出す
+  const toolContent = await client.request(
+    {
+      method: "tools/call",
+      params: {
+        name: "get-forecast",
+        arguments: {
+          latitude: 40.7128,
+          longitude: -74.006,
+        },
+      },
+    },
+    CallToolResultSchema
+  );
+  console.log("tool content:", toolContent);
 };
-main();
+
+main().catch((error) => {
+  console.error("Fatal error in main():", error);
+  process.exit(1);
+});
